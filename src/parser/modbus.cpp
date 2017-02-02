d630b66cf93e99073ab1df67cee7d10c143fdf7d
Joel <joel.anttila@outlook.com>
2017-02-02 03:46:13 +0200

    Adding form of representing the data as structure.
    With class managing it's memory, etc.

---
+++ src/parser/modbus.cpp
@@ -0,0 +1,23 @@
+#include "modbus.h"
+
+namespace modbit {
+Modbus::Modbus()
+{
+}
+
+Modbus::~Modbus()
+{
+	for(int i = 0; i < m_datatable.size(); i++)
+		delete m_datatable[i];
+}
+
+void Modbus::AddData(int reg, uint16_t val)
+{
+	auto ptr = new Data_t;
+	ptr->reg = reg;
+	ptr->val = val;
+
+	m_datatable.emplace_back(ptr);
+}
+
+}
--- src/parser/modbus.h
+++ src/parser/modbus.h
@@ -3,4 +3,31 @@
 
 #include "dataparser.h"
 
+namespace modbit {
+
+
+class Modbus {
+public:
+	Modbus();
+	~Modbus();
+
+	struct Data_t {
+		int reg;
+		uint16_t val;
+	};
+
+	/**
+	 * Could be easily sorted out with template if we would
+	 * follow the second option, but for now we are going to
+	 * represent the data as it is.
+	 */
+	void AddData(int reg, uint16_t val);
+
+	//std::vector<std::unique_ptr<Data_t>> &GetData() { return m_datatable; }
+
+private:
+	std::vector<Data_t*> m_datatable;
+};
+}
+
 #endif // !MODBUS_H
